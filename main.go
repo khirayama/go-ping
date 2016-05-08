4b1f629ccfe033d14491987c1470381af0a2411e
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-05-09 06:19:44 +0900

    Create getIpAddr

--- main.go
+++ main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"net"
 	"os"
 )
+
+func getIPAddr(host string) (net.IP, error) {
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, ip := range ips {
+		if ip.To4() != nil {
+			return ip.To4(), nil
+		}
+	}
+
+	return nil, errors.New("IP address not found")
+}
 
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "arg error")
 		os.Exit(1)
 	}
-	// host := os.Args[1]
+	host := os.Args[1]
+
+	// ip, err := getIPAddr(host)
+	ip, _ := getIPAddr(host)
+	fmt.Println(ip)
 }
